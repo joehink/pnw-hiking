fb2325129fbc116bf1a43e23a16f3726f861a14f
EJacobson96 <eric.jj96@gmail.com>
2018-06-14 17:14:16 -0700

    create map

--- client/App.js
+++ client/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import Map from './src/components/Map';
+
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Map 
+        markers = { [{
+          title: "My Marker",
+          desc: "Marker Desc"
+        }] } 
+      />
     );
   }
 }
---
+++ client/src/components/Map.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+import MapView from 'react-native-maps';
+
+class Map extends React.Component {
+  render() {
+    return (
+      <MapView style={styles.container}>
+        { 
+          this.props.markers.map((marker) => {
+            console.log(marker);
+            return <MapView.Marker
+              coordinate={{
+                latitude: 47.0248,
+                longitude: -120.9406
+              }}
+              image={require('../images/hiker.png')}
+              title={ marker.title }
+              description={ marker.desc }
+            />
+          })
+        }
+      </MapView>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default Map;
